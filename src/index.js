d89e140caf7a79d3f929d94f4205d7168034ffa9
jansesun <307665751@qq.com>
2017-09-24 17:11:30 +0800

    Fixed logic operation bug
    Merge xor and or methods
    add unit test case
    Support includes and remove methods
    add arguments validation
    
    Change-Id: I91d173b3add48b917f08e42ed500194ff424498a

--- src/index.js
+++ src/index.js
@@ -17,6 +17,18 @@ class BitMap {
     return ((runningLength + wordLength) << 5) - !runningLength;
   }
   _set(value) {
+    if(
+      typeof value !== 'number' ||
+      Number.isNaN(value) ||
+      !Number.isFinite(value) ||
+      value < 0 ||
+      ~~value !== value
+    ) {
+      if(typeof process === 'undefined' || process.env.NODE_ENV !== 'test') {
+        console.error('Value should be a native number');
+      }
+      return;
+    }
     let totalRunningLength = 0;
     let isSet = false;
     let upperBound = 0;
@@ -79,69 +91,41 @@ class BitMap {
     }
     return result;
   }
-  or(bitMap) {
-    const runningLengthSet = new Set();
-    bitMap = bitMap.bitMap;
-    const len1 = this.bitMap.length;
-    const len2 = bitMap.length;
-    for(let i = 0; i < len1;) {
-      runningLengthSet.add(this.bitMap[i + 1]);
+  remove(...data) {
+    const removeBitMap = new BitMap(...data);
+    this.bitMap = removeBitMap.xor(this.or(removeBitMap)).bitMap;
+  }
+  includes(value) {
+    if(
+      typeof value !== 'number' ||
+      Number.isNaN(value) ||
+      !Number.isFinite(value) ||
+      value < 0 ||
+      ~~value !== value
+    ) {
+      if(typeof process === 'undefined' || process.env.NODE_ENV !== 'test') {
+        console.error('Value should be a native number');
+      }
+      return false;
+    }
+    let upperBound = 0;
+    const bitValue = 1 << (value % 64);
+    for(let i = 0, len = this.bitMap.length; i < len;) {
+      upperBound += this._getUpperBound(this.bitMap[i + 1], this.bitMap[i]);
+      if(value <= upperBound) {
+        const index = (value - upperBound + (this.bitMap[i] << 5)) >> 5;
+        if(index >= 0) {
+          return !!(this.bitMap[i + index + 2] & bitValue);
+        } else {
+          return false;
+        }
+      }
       i += this.bitMap[i] + 2;
     }
-    for(let i = 0; i < len2;) {
-      runningLengthSet.add(bitMap[i + 1]);
-      i += bitMap[i] + 2;
-    }
-    const buffer = new ArrayBuffer((runningLengthSet.size + 1) << 4);
-    const result = new Uint32Array(buffer);
-    let upperBound1 = 0;
-    let upperBound2 = 0;
-    let upperBound = 0;
-    for(let i = 0, j = 0, k = 0; i < len1 || j < len2;) {
-      if(i < len1 && j < len2 && this.bitMap[i + 1] === bitMap[j + 1]) {
-        const runningLength1 = this.bitMap[i + 1];
-        const runningLength2 = bitMap[j + 1];
-        const wordLength = bitMap[j];
-        result[k] = wordLength;
-        result[k + 1] = ((runningLength2 << 5) + upperBound2 - upperBound) >> 5;
-        let offset = 2;
-        while(offset < wordLength + 2) {
-          result[k + offset] = bitMap[j + offset] | this.bitMap[i + offset];
-          offset++;
-        }
-        upperBound1 += this._getUpperBound(runningLength1, this.bitMap[i]);
-        upperBound2 += this._getUpperBound(runningLength2, bitMap[j]);
-        upperBound += this._getUpperBound(result[k + 1], result[k]);
-        i += offset;
-        j += offset;
-        k += offset;
-      } else if(j >= len2 || (i < len1 && this.bitMap[i + 1] < bitMap[j + 1])) {
-        const runningLength1 = this.bitMap[i + 1];
-        let offset = 0;
-        for(let len = this.bitMap[i] + 2; offset < len; offset++) {
-          result[k + offset] = this.bitMap[i + offset];
-        }
-        result[k + 1] = ((runningLength1 << 5) + upperBound1 - upperBound) >> 5;
-        upperBound1 += this._getUpperBound(runningLength1, this.bitMap[i]);
-        upperBound += this._getUpperBound(result[k + 1], result[k]);
-        i += offset;
-        k += offset;
-      } else {
-        const runningLength2 = bitMap[j + 1];
-        let offset = 0;
-        for(let len = bitMap[j] + 2; offset < len; offset++) {
-          result[k + offset] = bitMap[j + offset];
-        }
-        result[k + 1] = ((runningLength2 << 5) + upperBound2 - upperBound) >> 5;
-        upperBound2 += this._getUpperBound(runningLength2, bitMap[j]);
-        upperBound += this._getUpperBound(result[k + 1], result[k]);
-        j += offset;
-        k += offset;
-      }
-    }
-    const resultBitMap = new BitMap();
-    resultBitMap.bitMap = result;
-    return resultBitMap;
+    return false;
+  }
+  or(bitMap) {
+    return this.xor(bitMap, (a, b) => a | b);
   }
   and(bitMap) {
     const runningLengthSet = new Set();
@@ -149,14 +133,17 @@ class BitMap {
     bitMap = bitMap.bitMap;
     const len1 = this.bitMap.length;
     const len2 = bitMap.length;
+    let upperBound = 0;
     for(let i = 0; i < len1;) {
-      runningLengthSet.add(this.bitMap[i + 1]);
+      upperBound += this._getUpperBound(this.bitMap[i + 1], this.bitMap[i]);
+      runningLengthSet.add(upperBound);
       i += this.bitMap[i] + 2;
     }
+    upperBound = 0;
     for(let i = 0; i < len2;) {
-      const runningLength = bitMap[i + 1];
-      if(runningLengthSet.has(runningLength)) {
-        intersection.add(runningLength);
+      upperBound += this._getUpperBound(bitMap[i + 1], bitMap[i]);
+      if(runningLengthSet.has(upperBound)) {
+        intersection.add(upperBound);
       }
       i += bitMap[i] + 2;
     }
@@ -164,9 +151,9 @@ class BitMap {
     const result = new Uint32Array(buffer);
     let upperBound1 = 0;
     let upperBound2 = 0;
-    let upperBound = 0;
+    upperBound = 0;
     for(let i = 0, j = 0, k = 0; i < len1 && j < len2;) {
-      if(this.bitMap[i + 1] === bitMap[j + 1]) {
+      if(upperBound1 + this._getUpperBound(this.bitMap[i + 1], this.bitMap[i]) === upperBound2 + this._getUpperBound(bitMap[j + 1], bitMap[j])) {
         const runningLength1 = this.bitMap[i + 1];
         const runningLength2 = bitMap[j + 1];
         const wordLength = bitMap[j];
@@ -183,7 +170,7 @@ class BitMap {
         i += offset;
         j += offset;
         k += offset;
-      } else if(this.bitMap[i + 1] < bitMap[j + 1]) {
+      } else if(upperBound1 + this._getUpperBound(this.bitMap[i + 1], this.bitMap[i]) < upperBound2 + this._getUpperBound(bitMap[j + 1], bitMap[j])) {
         i += this.bitMap[i] + 2;
       } else {
         j += bitMap[j] + 2;
@@ -193,27 +180,31 @@ class BitMap {
     resultBitMap.bitMap = result;
     return resultBitMap;
   }
-  xor(bitMap) {
+  xor(bitMap, callback = (a, b) => a ^ b) {
     const runningLengthSet = new Set();
     bitMap = bitMap.bitMap;
     const len1 = this.bitMap.length;
     const len2 = bitMap.length;
+    let upperBound = 0;
     for(let i = 0; i < len1;) {
-      runningLengthSet.add(this.bitMap[i + 1]);
+      upperBound += this._getUpperBound(this.bitMap[i + 1], this.bitMap[i]);
+      runningLengthSet.add(upperBound);
       i += this.bitMap[i] + 2;
     }
+    upperBound = 0;
     for(let i = 0; i < len2;) {
-      runningLengthSet.add(bitMap[i + 1]);
+      upperBound += this._getUpperBound(bitMap[i + 1], bitMap[i]);
+      runningLengthSet.add(upperBound);
       i += bitMap[i] + 2;
     }
     const buffer = new ArrayBuffer((runningLengthSet.size + 1) << 4);
     const result = new Uint32Array(buffer);
     let upperBound1 = 0;
     let upperBound2 = 0;
-    let upperBound = 0;
+    upperBound = 0;
     let k = 0;
     for(let i = 0, j = 0; i < len1 || j < len2;) {
-      if(i < len1 && j < len2 && this.bitMap[i + 1] === bitMap[j + 1]) {
+      if(i < len1 && j < len2 && upperBound1 + this._getUpperBound(this.bitMap[i + 1], this.bitMap[i]) === upperBound2 + this._getUpperBound(bitMap[j + 1], bitMap[j])) {
         const runningLength1 = this.bitMap[i + 1];
         const runningLength2 = bitMap[j + 1];
         const wordLength = bitMap[j];
@@ -222,7 +213,7 @@ class BitMap {
         let offset = 2;
         let isAllZero = true;
         while(offset < wordLength + 2) {
-          result[k + offset] = bitMap[j + offset] ^ this.bitMap[i + offset];
+          result[k + offset] = callback(bitMap[j + offset], this.bitMap[i + offset]);
           if(result[k + offset]) {
             isAllZero = false;
           }
@@ -236,7 +227,7 @@ class BitMap {
           upperBound += this._getUpperBound(result[k + 1], result[k]);
           k += offset;
         }
-      } else if(j >= len2 || (i < len1 && this.bitMap[i + 1] < bitMap[j + 1])) {
+      } else if(j >= len2 || (i < len1 && upperBound1 + this._getUpperBound(this.bitMap[i + 1], this.bitMap[i]) < upperBound2 + this._getUpperBound(bitMap[j + 1], bitMap[j]))) {
         const runningLength1 = this.bitMap[i + 1];
         let offset = 0;
         for(let len = this.bitMap[i] + 2; offset < len; offset++) {
--- test/index.spec.js
+++ test/index.spec.js
@@ -2,32 +2,70 @@ import expect from 'expect';
 import BitMap from '../src/index';
 
 describe('BitMap', () => {
-  const bitMap = new BitMap();
-  it('Should set data correctly', () => {
-    bitMap.set(1);
-    expect(bitMap.get()).toEqual([1]);
-    bitMap.set(4);
-    expect(bitMap.get()).toEqual([1, 4]);
-    bitMap.set(64);
-    expect(bitMap.get()).toEqual([1, 4, 64]);
-    bitMap.set(129);
-    expect(bitMap.get()).toEqual([1, 4, 64, 129]);
-  });
-  it('Should extend currectly', () => {
-    bitMap.set(400000);
-    expect(bitMap.get()).toEqual([1, 4, 64, 129, 400000]);
-    bitMap.set(400003);
-    expect(bitMap.get()).toEqual([1, 4, 64, 129, 400000, 400003]);
-  });
-  it('Should insert currectly', () => {
-    bitMap.set(200000);
-    expect(bitMap.get()).toEqual([1, 4, 64, 129, 200000, 400000, 400003]);
+  describe('set', () => {
+    const bitMap = new BitMap();
+    it('Should set data correctly', () => {
+      bitMap.set(1);
+      expect(bitMap.get()).toEqual([1]);
+      bitMap.set(4);
+      expect(bitMap.get()).toEqual([1, 4]);
+      bitMap.set(64);
+      expect(bitMap.get()).toEqual([1, 4, 64]);
+      bitMap.set(129);
+      expect(bitMap.get()).toEqual([1, 4, 64, 129]);
+    });
+    it('Should not accept invalid value', () => {
+      const bitMap = new BitMap();
+      bitMap.set('1');
+      expect(bitMap.get()).toEqual([]);
+      bitMap.set(+'a');
+      expect(bitMap.get()).toEqual([]);
+      bitMap.set(-1);
+      expect(bitMap.get()).toEqual([]);
+      bitMap.set(1.1);
+      expect(bitMap.get()).toEqual([]);
+      bitMap.set(1 / 0);
+      expect(bitMap.get()).toEqual([]);
+    });
+    it('Should extend currectly', () => {
+      bitMap.set(400000);
+      expect(bitMap.get()).toEqual([1, 4, 64, 129, 400000]);
+      bitMap.set(400003);
+      expect(bitMap.get()).toEqual([1, 4, 64, 129, 400000, 400003]);
+    });
+    it('Should insert currectly', () => {
+      bitMap.set(200000);
+      expect(bitMap.get()).toEqual([1, 4, 64, 129, 200000, 400000, 400003]);
+    });
   });
   describe('or', () => {
     it('Should or login currectly', () => {
       const bitMap1 = new BitMap(1, 129, 64, 200000);
       const bitMap2 = new BitMap(1, 4, 64, 400000, 400003);
       expect(bitMap1.or(bitMap2).get()).toEqual([1, 4, 64, 129, 200000, 400000, 400003]);
+    });
+    it('Should conform to the idempotent law', () => {
+      const bitMap1 = new BitMap(1, 129, 64, 200000);
+      const bitMap2 = new BitMap(1, 129, 64, 200000);
+      expect(bitMap1.or(bitMap2).get()).toEqual(bitMap1.get());
+    });
+    it('Should conform to the commutative law', () => {
+      const bitMap1 = new BitMap(1, 129, 64, 200000);
+      const bitMap2 = new BitMap(1, 4, 64, 400000, 400003);
+      expect(bitMap1.or(bitMap2).get()).toEqual(bitMap2.or(bitMap1).get());
+    });
+    it('Should conform to the associative law', () => {
+      const bitMap1 = new BitMap(1, 129, 64, 200000);
+      const bitMap2 = new BitMap(1, 4, 64, 400000, 400003);
+      const bitMap3 = new BitMap(4, 64, 400000);
+      expect(bitMap1.or(bitMap2).or(bitMap3).get()).toEqual(bitMap1.or(bitMap2.or(bitMap3)).get());
+    });
+    it('Should conform to the complementation law', () => {
+      const bitMap1 = new BitMap(1, 129, 64, 200000);
+      const bitMap2 = new BitMap(1, 4, 64, 400000, 400003);
+      const full = bitMap1.or(bitMap2);
+      const complementation = bitMap1.xor(full);
+      expect(bitMap1.or(complementation).get()).toEqual(full.get());
     });
   });
   describe('and', () => {
@@ -36,12 +74,70 @@ describe('BitMap', () => {
       const bitMap2 = new BitMap(1, 4, 64, 400000, 400003);
       expect(bitMap1.and(bitMap2).get()).toEqual([1, 64]);
     });
+    it('Should conform to the idempotent law', () => {
+      const bitMap1 = new BitMap(1, 129, 64, 200000);
+      const bitMap2 = new BitMap(1, 129, 64, 200000);
+      expect(bitMap1.and(bitMap2).get()).toEqual(bitMap1.get());
+    });
+    it('Should conform to the commutative law', () => {
+      const bitMap1 = new BitMap(1, 129, 64, 200000);
+      const bitMap2 = new BitMap(1, 4, 64, 400000, 400003);
+      expect(bitMap1.and(bitMap2).get()).toEqual(bitMap2.and(bitMap1).get());
+    });
+    it('Should conform to the associative law', () => {
+      const bitMap1 = new BitMap(1, 129, 64, 200000);
+      const bitMap2 = new BitMap(1, 4, 64, 400000, 400003);
+      const bitMap3 = new BitMap(4, 64, 400000);
+      expect(bitMap1.and(bitMap2).and(bitMap3).get()).toEqual(bitMap1.and(bitMap2.and(bitMap3)).get());
+    });
+    it('Should conform to the complementation law', () => {
+      const bitMap1 = new BitMap(1, 129, 64, 200000);
+      const bitMap2 = new BitMap(1, 4, 64, 400000, 400003);
+      const full = bitMap1.or(bitMap2);
+      const complementation = bitMap1.xor(full);
+      expect(bitMap1.and(complementation).get()).toEqual(new BitMap().get());
+    });
+  });
+  describe('and, or', () => {
+    it('Should conform to the absorption law', () => {
+      const bitMap1 = new BitMap(1, 129, 64, 200000);
+      const bitMap2 = new BitMap(1, 4, 64, 400000, 400003);
+      expect(bitMap1.and(bitMap1.or(bitMap2)).get()).toEqual(bitMap1.get());
+      expect(bitMap1.or(bitMap1.and(bitMap2)).get()).toEqual(bitMap1.get());
+    });
   });
   describe('xor', () => {
     it('Should xor login currectly', () => {
       const bitMap1 = new BitMap(1, 129, 64, 200000);
       const bitMap2 = new BitMap(1, 4, 64, 400000, 400003, 129, 200000);
       expect(bitMap1.xor(bitMap2).get()).toEqual([4, 400000, 400003]);
+    });
+    it('Should conform to the commutative law', () => {
+      const bitMap1 = new BitMap(1, 129, 64, 200000);
+      const bitMap2 = new BitMap(1, 4, 64, 400000, 400003);
+      expect(bitMap1.xor(bitMap2).get()).toEqual(bitMap2.xor(bitMap1).get());
+    });
+    it('Should conform to the associative law', () => {
+      const bitMap1 = new BitMap(1, 129, 64, 200000);
+      const bitMap2 = new BitMap(1, 4, 64, 400000, 400003);
+      const bitMap3 = new BitMap(4, 64, 400000);
+      expect(bitMap1.xor(bitMap2).xor(bitMap3).get()).toEqual(bitMap1.xor(bitMap2.xor(bitMap3)).get());
+    });
+  });
+  describe('remove', () => {
+    it('Should remove data correctly', () => {
+      const bitMap1 = new BitMap(1, 4, 64, 400000, 400003);
+      bitMap1.remove(1, 129, 64, 200000)
+      expect(bitMap1.get()).toEqual([4, 400000, 400003]);
+    });
+  });
+  describe('includes', () => {
+    const bitMap = new BitMap(1, 4, 200000, 400000);
+    it('Should recognize the value exist in the map', () => {
+      expect(bitMap.includes(200000)).toBe(true);
+    });
+    it('Should recognize the value not exist in the map', () => {
+      expect(bitMap.includes(200001)).toBe(false);
     });
   });
 })
